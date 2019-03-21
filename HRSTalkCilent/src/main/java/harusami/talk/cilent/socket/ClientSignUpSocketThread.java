49fcc05236d9165f9d9d4bc96fb87c1a1ff38d7d
rinne <37577161+rinne@users.noreply.github.com>
2019-03-22 03:10:19 +0800

    增加Scoket多线程聊天功能

---
+++ HRSTalkCilent/src/main/java/harusami/talk/cilent/socket/ClientSignUpSocketThread.java
@@ -0,0 +1,20 @@
+package harusami.talk.cilent.socket;
+/*
+ * @package: harusami.talk.cilent.socket
+ * @program: HRSTalk
+ * @description  ${description}
+ *
+ * @author:  rinne
+ * @e-mail:  minami.rinne.me@gmail.com
+ * @date: 2019/03/22 上午 03:01
+ */
+
+/**
+ * @classname: ClientSignUpSocketThread
+ * @description: %{description}
+ * @author: rinne
+ * @date: 2019/03/22 上午 03:01
+ * @Version 1.0
+ */
+public class ClientSignUpSocketThread {
+}
